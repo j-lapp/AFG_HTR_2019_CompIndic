2ffcc27acd71022427adb14e9fe44b9e6edb5616
Martin <martin.barner@impact-initiatives.org>
2019-09-25 19:02:46 +0200

    top anks

---
+++ top_ranks.R
@@ -0,0 +1,50 @@
+coerc<-function(x){as.numeric(as.character(x))}
+
+
+
+
+
+
+# load & prep inputs -------------------------------------------------------------
+
+data <- read.csv("input/data/HTR_2019_round1_merged_cleaned_master_2019-09-24.csv", stringsAsFactors = F, na.strings = c("", "NA"))
+weight<-read.csv("input/weights/REACH_AFG_H2R_R1_weights_0924.csv", stringsAsFactors = F, na.strings = c("", "NA"))
+
+data<-full_join(data, weight,by = c("district_reporting"="district"))
+data$weight<-coerc(data[["num_interviews"]])/coerc(data[["total_villages"]])
+
+
+
+# prep indicators ---------------------------------------------------------
+
+
+
+priority_needs<-data %>%
+  select(starts_with("priority_needs"),weight) %>%
+  select(-priority_needs) %>% as_tibble
+
+
+priority_needs %>% lapply(weighted.mean,priority_needs$weight,na.rm = T) %>%
+  t %>% t %>% write.csv("priority_needs.csv")
+
+
+
+type_assistance<-data %>%
+  select(starts_with("type_assistance"),weight) %>%
+  select(-type_assistance) %>% as_tibble
+
+
+type_assistance %>% lapply(weighted.mean,type_assistance$weight,na.rm=T) %>%
+  t %>% t %>% write.csv("type_assistance.csv")
+  
+
+
+
+
+
+
+
+
+
+
+    
